6f762690f1b4e58cc064004d24091c425347757d
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-10-02 23:50:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,9 +8,9 @@ def run_guessing_game
   number =  rand(6) + 1
     if input == "exit"
       puts "Goodbye!"
-    elsif (1..6).to_a.include?(input.to_i) 
-      puts "You guessed the correct number!"
-    else
+    elsif (1..6).to_a.!include?(input.to_i) 
       puts "Sorry! The computer guessed #{number}"
+    else
+      puts "You guessed the correct number!"
   end
 end
